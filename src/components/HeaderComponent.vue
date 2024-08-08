6b064926c0c710e367bd73fb7843613ca47eba09
milcho0604 <milcho0604@gmail.com>
2024-08-08 23:08:48 +0900

    feat: try Sse notification

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -27,7 +27,9 @@
 </template>
 
 <script>
+import { EventSourcePolyfill } from 'event-source-polyfill';
 import { mapGetters } from 'vuex';
+// 서버와 실시간 알림 서비스를 위한 의존성 추가
 
 
 export default {
@@ -46,6 +48,14 @@ export default {
         this.isLogin = true;
         this.userRole = localStorage.getItem('role');
     }
+    // sse 연결을 위한 코드
+    if(this.userRole === 'ADMIN'){
+      let sse = new EventSourcePolyfill(`${process.env.VUE_APP_API_BASE_URL}/subscribe`, 
+      {headers: {Authorization: `Bearer ${token}`}}
+    );
+      sse.addEventListener('connect', (event) => { console.log(event) })
+    }
+
   },
   methods:{
     doLogout(){
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -67,7 +67,7 @@ export default {
                 // this.$router.push("/");
                 window.location.href = "/";
             } catch (error) {
-                console.error('로그인 실패:', error.response.data);
+                console.error('로그인 실패:', error);
                 alert('로그인에 실패했습니다. 다시 시도해주세요.');
             }
         },
